38f4b1294c99e26c8c30983be868611ec7e93af0
Sandoche <sandoche@gmail.com>
2022-09-30 11:09:55 +0100

    Fixing bad renaming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Layout from '../components/main';
+import MainLayout from '../components/MainLayout';
 import Header from '../components/Header';
 
 function Index() {
   return (
-    <Layout>
+    <MainLayout>
       <Header />
       <div className="first-step" style={{ maxWidth: '750px' }}>
         <p>
           If you are looking for the KYC Form we invite you to <a href="mailto:hello@near.foundation">contact the NEAR Foundation</a>.
         </p>
       </div>
-    </Layout>
+    </MainLayout>
   );
 }
 export default Index;
