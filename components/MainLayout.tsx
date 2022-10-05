c7b38271f29ca8ec5d15b4142fb0a50bf80576aa
Sandoche Adittane <sandoche@gmail.com>
2022-10-05 15:43:18 +0100

    Fixing warning issues

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -8,15 +8,6 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
     <div>
       <Head>
         <title>NEAR Identity Verification</title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
-        />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
       </Head>
 
       <main>{children} </main>
