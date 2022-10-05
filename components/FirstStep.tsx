b6fe0588b7df741019fe6f3035420769b22b5d3f
Sandoche Adittane <sandoche@gmail.com>
2022-10-05 15:40:18 +0100

    Fixing invalid character

--- components/FirstStep.tsx
+++ components/FirstStep.tsx
@@ -12,7 +12,7 @@ function FirstStep({ onfidoInstance, onSubmit }: { onfidoInstance: SdkHandle | n
       <div className="first-step" style={{ maxWidth: '500px' }}>
         <h3 className="mb-4">We want to get to know you!</h3>
         <p>Start by introducing yourself here.</p>
-        <p>On the next page, we&#39ll ask you to provide other information (documents or photos) that will help verify your identity.</p>
+        <p>On the next page, we&apos;ll ask you to provide other information (documents or photos) that will help verify your identity.</p>
 
         <ApplicantForm onSubmit={onSubmit} />
       </div>
