d96e8f6454c04b8702b95320ef08e7a589eeb866
Sandoche <sandoche@gmail.com>
2022-10-06 10:28:59 +0100

    New result page created

---
+++ pages/[key]/result.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+
+import MainLayout from '../../components/MainLayout';
+
+const ResultPage: NextPage = () => {
+  return (
+    <MainLayout>
+      <div>
+        <h1>Result</h1>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default ResultPage;
