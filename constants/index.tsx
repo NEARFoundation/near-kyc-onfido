e58f818a14b82b95785e3f69d07b2cbb84043f90
Sandoche <sandoche@gmail.com>
2022-10-06 16:43:18 +0100

    Adding user data in cookies

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,4 +1,5 @@
 export const COOKIES_EXPIRATION_TIME = 60 * 60 * 24 * 30; // 30 days
 export const COOKIE_CHECK_ID_NAME = 'onfido-check-id';
+export const COOKIE_USER_DATA_NAME = 'onfido-user-data';
 export const SHORT_POLLING_INTERVAL = 1000;
 export const LONG_POLLING_INTERVAL = 30000;
