964543cb4d1c354a62e6c22f3ea3a371ac9f61b0
Sandoche <sandoche@gmail.com>
2022-10-06 10:24:46 +0100

    Adding Loading spinner component

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner(): JSX.Element {
+  return (
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
