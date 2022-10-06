70f9e022d16a79e3d09bdaa4464f3a0d46d47e33
Sandoche <sandoche@gmail.com>
2022-10-06 11:18:46 +0100

    Loading block added

--- pages/[key]/result.tsx
+++ pages/[key]/result.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next';
 
+import LoadingSpinner from '../../components/LoadingSpinner';
 import MainLayout from '../../components/MainLayout';
 
 const ResultPage: NextPage = () => {
   return (
     <MainLayout>
       <div className="result-box">
-        <h1>Result</h1>
+        <div className="loading-centered">
+          <LoadingSpinner />
+          <p className="mt-2">Please wait...</p>
+        </div>
       </div>
     </MainLayout>
   );
