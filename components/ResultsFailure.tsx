e90e078d958d4897570a2460b6aecf5065725c0c
Sandoche <sandoche@gmail.com>
2022-10-06 15:53:01 +0100

    Improving button style

--- components/ResultsFailure.tsx
+++ components/ResultsFailure.tsx
@@ -7,8 +7,8 @@ export default function ResultsFailure(): JSX.Element {
     <div className="block-centered">
       <i className="fa  fa-times-circle text-danger mb-4" style={styles} aria-hidden="true" />
       <h1 className="fs-2 mb-2">Verification failed</h1>
-      <p className="text-secondary">Sorry, we invite you to try again the verification.</p>
-      <button name="submit" type="submit" className="btn btn-lg btn-primary">
+      <p className="text-secondary mb-4">Sorry, we invite you to try again the verification.</p>
+      <button name="submit" type="submit" className="btn btn-primary">
         Try again <i className="fa fa-repeat" aria-hidden="true" />
       </button>
     </div>
