37f3da57c08f4e4d5edb2b04f615bbc1af6704b4
Sandoche <sandoche@gmail.com>
2022-10-06 13:11:29 +0100

    Creating dummy endpoint

---
+++ pages/api/check-results.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import type CheckResults from '../../types/CheckResults';
+
+// import getOnfido from '../../helpers/onfido';
+
+// const endpointName = 'check-results';
+
+// const onfido = getOnfido();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<CheckResults>) {
+  res.status(200).json({
+    isClear: true,
+  });
+}
---
+++ types/CheckResults.ts
@@ -0,0 +1,5 @@
+interface CheckResults {
+  isClear: boolean;
+}
+
+export default CheckResults;
