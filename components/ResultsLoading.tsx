38ad8b7258e3b8ad57eaea9233e4bda1ee1aed94
Sandoche <sandoche@gmail.com>
2022-10-06 15:39:21 +0100

    Success component implemented

--- components/ResultsLoading.tsx
+++ components/ResultsLoading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from './LoadingSpinner';
 
 export default function ResultsLoading(): JSX.Element {
   return (
-    <div className="loading-centered">
+    <div className="block-centered">
       <LoadingSpinner />
       <p className="mt-2">Please wait...</p>
     </div>
--- components/ResultsSuccess.tsx
+++ components/ResultsSuccess.tsx
@@ -1,3 +1,13 @@
+const styles = {
+  fontSize: '98px',
+};
+
 export default function ResultsSuccess(): JSX.Element {
-  return <div>Success</div>;
+  return (
+    <div className="block-centered">
+      <i className="fa fa-check-circle text-success mb-4" style={styles} aria-hidden="true" />
+      <h1 className="fs-2 mb-2">KYC Passed successfully</h1>
+      <p className="text-secondary">You can now close this window. The NEAR Foundation will contact you for the next steps.</p>
+    </div>
+  );
 }
