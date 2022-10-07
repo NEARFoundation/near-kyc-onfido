0d87e752529f06ce314cbf46b2ad77127fd4c358
Sandoche <sandoche@gmail.com>
2022-10-07 11:43:31 +0100

    Improving style of landing page

--- components/form/FirstStep.tsx
+++ components/form/FirstStep.tsx
@@ -1,5 +1,6 @@
 import type { SdkHandle } from 'onfido-sdk-ui';
 
+import CenteredCard from '../common/CenteredCard';
 import Header from '../layout/Header';
 
 import ApplicantForm from './ApplicantForm';
@@ -8,16 +9,14 @@ function FirstStep({ onfidoInstance, onSubmit, loading }: { onfidoInstance: SdkH
   return onfidoInstance ? (
     <div />
   ) : (
-    <>
+    <CenteredCard>
       <Header />
-      <div className="first-step" style={{ maxWidth: '500px' }}>
-        <h3 className="mb-4">We want to get to know you!</h3>
-        <p>Start by introducing yourself here.</p>
-        <p>On the next page, we&apos;ll ask you to provide other information (documents or photos) that will help verify your identity.</p>
+      <h3 className="mb-4">We want to get to know you!</h3>
+      <p>Start by introducing yourself here.</p>
+      <p>On the next page, we&apos;ll ask you to provide other information (documents or photos) that will help verify your identity.</p>
 
-        <ApplicantForm onSubmit={onSubmit} loading={loading} />
-      </div>
-    </>
+      <ApplicantForm onSubmit={onSubmit} loading={loading} />
+    </CenteredCard>
   );
 }
 
