2f4ed1d15f6e5251949de36d3cbe33e7f353d72b
Sandoche Adittane <sandoche@gmail.com>
2022-10-10 11:39:51 +0100

    Rewriting positive message

--- components/results/ResultsSuccess.tsx
+++ components/results/ResultsSuccess.tsx
@@ -4,7 +4,7 @@ export default function ResultsSuccess(): JSX.Element {
   return (
     <CenteredCardContent
       title="Verification validated"
-      description="You can now close this window. The NEAR Foundation will contact you for the next steps."
+      description="Congratulations! 🎉 We have verified your identity and are excited to move forward. NEAR Foundation will contact you for the next steps shortly. You can now close this window."
       iconClasses="fa fa-check-circle text-success mb-4"
     />
   );
