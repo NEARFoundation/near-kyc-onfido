141508475b70b00a05edcc754a28014fe915b236
Sandoche Adittane <sandoche@gmail.com>
2022-10-10 11:35:48 +0100

    Changing failure text to make it more clear

--- components/results/ResultsFailure.tsx
+++ components/results/ResultsFailure.tsx
@@ -4,7 +4,11 @@ import ResultsRetryButton from './ResultsRetryButton';
 
 export default function ResultsFailure(): JSX.Element {
   return (
-    <CenteredCardContent title="Verification failed" description="Sorry, we invite you to try again the verification." iconClasses="fa fa-times-circle text-danger mb-4">
+    <CenteredCardContent
+      title="Verification failed"
+      description="We could not verify your identity. If you believe that you submitted any information incorrectly, you may try again."
+      iconClasses="fa fa-times-circle text-danger mb-4"
+    >
       <ResultsRetryButton />
     </CenteredCardContent>
   );
