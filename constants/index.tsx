9b5cec9d0cd2ab59964874466cfce281e2e4105f
Sandoche Adittane <sandoche@gmail.com>
2022-10-10 11:41:56 +0100

    Reformatting values and adding comments for the constants

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,5 +1,5 @@
 export const COOKIES_EXPIRATION_TIME = 60 * 60 * 24 * 30; // 30 days
 export const COOKIE_CHECK_ID_NAME = 'onfido-check-id';
 export const LOCALSTORAGE_USER_DATA_NAME = 'onfido-user-data';
-export const SHORT_POLLING_INTERVAL = 1000;
-export const LONG_POLLING_INTERVAL = 30000;
+export const SHORT_POLLING_INTERVAL = 1000; // 1 second - Short polling applies when the check is in progress
+export const LONG_POLLING_INTERVAL = 30_000; // 30 seconds - Long polling applies when the check is under manual approval
