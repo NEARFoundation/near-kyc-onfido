9afdc5aa65c29c09ec1d2ecdcdc3bc3cab5b4a6d
Sandoche <sandoche@protonmail.com>
2022-11-03 16:33:28 +0000

    Adding error boundary

--- components/common/Error500.tsx
+++ components/common/Error500.tsx
@@ -1,23 +1,21 @@
-import Link from 'next/link';
+import MainLayout from '../layout/MainLayout';
+import ResultsRetryButton from '../results/ResultsRetryButton';
 
 import CenteredCard from './CenteredCard';
 import CenteredCardContent from './CenteredCardContent';
 
 export default function Error500(): JSX.Element {
   return (
-    <CenteredCard>
-      <CenteredCardContent
-        title="An error occured"
-        description="Sorry, an error occurred, we invite you to refresh the page"
-        iconClasses="fa fa-exclamation-circle text-warning mb-4"
-      >
-        <Link href={`/${process.env.NEXT_PUBLIC_KYC_ENDPOINT_KEY}`} passHref>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="btn btn-primary">
-            Try again <i className="fa fa-repeat" aria-hidden="true" />
-          </a>
-        </Link>
-      </CenteredCardContent>
-    </CenteredCard>
+    <MainLayout>
+      <CenteredCard>
+        <CenteredCardContent
+          title="An error occured"
+          description="Sorry, an error occurred, we invite you to refresh the page"
+          iconClasses="fa fa-exclamation-circle text-warning mb-4"
+        >
+          <ResultsRetryButton autoRetry={false} />
+        </CenteredCardContent>
+      </CenteredCard>
+    </MainLayout>
   );
 }
