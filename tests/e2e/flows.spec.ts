bddeff4f3b27d4105aa1cb32c01c371b743ff996
Sandoche <sandoche@protonmail.com>
2022-11-14 16:18:02 +0000

    Installing the dependencies and moving files around

--- tests/e2e/flows.spec.ts
+++ tests/e2e/flows.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { Browser, BrowserContext, chromium, expect, test } from '@playwright/test';
 
-import type ApplicantProperties from '../types/ApplicantProperties';
+import type ApplicantProperties from '../../types/ApplicantProperties';
 
 import { MOCK_VIDEO_PATH } from './utils/constants';
 import { continueOnfidoFlowThenGetAndTestLink, fillStartForm, openKycLinkAndTestDocumentAndPhotoScan, submittingDocuments } from './utils/helpers';
@@ -44,7 +44,7 @@ test('Applicant should be able to fill the form with a browser, submit documents
 
   await submittingDocuments(desktopPage);
   await expect(desktopPage.getByRole('heading', { name: /Verification/i })).toHaveText('Verification validated');
-  await desktopPage.screenshot({ path: 'tests/screenshots/validated.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/validated.png', fullPage: true });
 });
 
 test('Applicant should be able to fill the form with a browser, submit documents and photo with a phone and see a Verification failed message and be able to retry', async () => {
@@ -58,7 +58,7 @@ test('Applicant should be able to fill the form with a browser, submit documents
 
   await submittingDocuments(desktopPage);
   await expect(desktopPage.getByRole('heading', { name: /Verification/i })).toHaveText('Verification failed');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed.png', fullPage: true });
 
   await desktopPage.getByRole('link', { name: 'Try again' }).click();
   await expect(desktopPage.getByText(/Verify your identity/i)).toHaveText('Verify your identity');
@@ -76,7 +76,7 @@ test('Upon submitting an unsupported document, applicant should see an error mes
   await submittingDocuments(desktopPage);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText('The document provided is not supported');
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_image_integrity_supported_docs.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_image_integrity_supported_docs.png', fullPage: true });
 });
 
 test('Upon submitting picturew with quality issues, applicant should see an error message about the pictures having quality issues', async () => {
@@ -93,7 +93,7 @@ test('Upon submitting picturew with quality issues, applicant should see an erro
     'The picture(s) you provided have quality issues, which may include: blurriness, darkness, glare, obstruction, etc.',
   );
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_image_integrity_image_quality.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_image_integrity_image_quality.png', fullPage: true });
 });
 
 test('Upon submitting an non-readable document, applicant should see an error message about the document not being readable', async () => {
@@ -109,7 +109,7 @@ test('Upon submitting an non-readable document, applicant should see an error me
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/There are issues with the pictures you provided/);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The document provided is not readable/);
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_visual_authenticity_fonts.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_visual_authenticity_fonts.png', fullPage: true });
 });
 
 test('Upon submitting an non authentic document, applicant should see an error message about the document not being authentic', async () => {
@@ -125,7 +125,7 @@ test('Upon submitting an non authentic document, applicant should see an error m
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/There are issues with the pictures you provided/);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The documents provided are not valid/);
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_visual_authenticity_security_features.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_visual_authenticity_security_features.png', fullPage: true });
 });
 
 test('Upon submitting an document without a clear face, applicant should see an error message about the document not having an identifiable face', async () => {
@@ -141,7 +141,7 @@ test('Upon submitting an document without a clear face, applicant should see an 
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/There are issues with the pictures you provided/);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The face could not be detected/);
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_visual_authenticity_face_detection.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_visual_authenticity_face_detection.png', fullPage: true });
 });
 
 test('Upon submitting a document with wrong numbers, applicant should see an error message about the document having invalid numbers', async () => {
@@ -157,7 +157,7 @@ test('Upon submitting a document with wrong numbers, applicant should see an err
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The data from your documents contains some errors/);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The document numbers you provided are invalid/);
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_data_validation_document_numbers.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_data_validation_document_numbers.png', fullPage: true });
 });
 
 test('Upon submitting an invalid document type, applicant should see an error message about the document having an invalid type', async () => {
@@ -173,7 +173,7 @@ test('Upon submitting an invalid document type, applicant should see an error me
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The document provided does not match the information provided/);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText(/The document type you provided is invalid/);
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_data_consistency_document_type.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_data_consistency_document_type.png', fullPage: true });
 });
 
 test('Upon submitting a fake document, applicant should see an error message about the document not being authentic', async () => {
@@ -188,7 +188,7 @@ test('Upon submitting a fake document, applicant should see an error message abo
   await submittingDocuments(desktopPage);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText('The picture provided does not seem authentic');
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_visual_authenticity_spoofing_detection.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_visual_authenticity_spoofing_detection.png', fullPage: true });
 });
 
 test('Upon submitting a photo different from the documents, applicant should see an error message about the photo not matching the document', async () => {
@@ -203,7 +203,7 @@ test('Upon submitting a photo different from the documents, applicant should see
   await submittingDocuments(desktopPage);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText('The picture does not match the document provided');
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_face_comparison_face_match.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_face_comparison_face_match.png', fullPage: true });
 });
 
 test('Upon submitting an invalid photo, applicant should see an error message about the photo being invalid', async () => {
@@ -218,7 +218,7 @@ test('Upon submitting an invalid photo, applicant should see an error message ab
   await submittingDocuments(desktopPage);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText('The photo provided is not valid');
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_image_integrity_source_integrity.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_image_integrity_source_integrity.png', fullPage: true });
 });
 
 test('Upon submitting a photo with another face, applicant should see an error message about the many faces detected in the photo', async () => {
@@ -233,5 +233,5 @@ test('Upon submitting a photo with another face, applicant should see an error m
   await submittingDocuments(desktopPage);
   await expect(await desktopPage.getByRole('list', { name: 'error list' })).toHaveText('Another face has been detected in the picture');
   await expect(desktopPage.getByText(/We could not verify your identity/)).toHaveText('We could not verify your identity. We invite you to read the reasons below and try again.');
-  await desktopPage.screenshot({ path: 'tests/screenshots/failed_image_integrity_face_detected.png', fullPage: true });
+  await desktopPage.screenshot({ path: 'tests/e2e/screenshots/failed_image_integrity_face_detected.png', fullPage: true });
 });
