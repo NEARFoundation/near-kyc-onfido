d0fac191e46e925fe8e792facb6478b02206fa46
Sandoche <sandoche@protonmail.com>
2022-11-14 15:16:03 +0000

    Displaying the swagger doc only in dev

--- pages/api/doc.ts
+++ pages/api/doc.ts
@@ -1,8 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { withSwagger } from 'next-swagger-doc';
 
 import packageJson from '../../package.json';
+import { NOT_FOUND } from '../../utils/statusCodes';
 
 const { version } = packageJson;
+
+const productionHandler = (req: NextApiRequest, res: NextApiResponse<{ status: string; code: number }>) => {
+  res.status(NOT_FOUND).send({ status: 'Endpoint not found', code: NOT_FOUND });
+};
 
 const swaggerHandler = withSwagger({
   definition: {
@@ -15,4 +21,4 @@ const swaggerHandler = withSwagger({
   apiFolder: 'pages/api',
 });
 
-export default swaggerHandler();
+export default process.env.NODE_ENV === 'production' ? productionHandler : swaggerHandler();
