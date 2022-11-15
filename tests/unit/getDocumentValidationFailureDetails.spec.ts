6191648714665b572c145a784f15650799e2bc27
Sandoche <sandoche@protonmail.com>
2022-11-15 11:48:36 +0000

    Work in progress getDocumentValidationFailureDetails tests

--- tests/unit/getDocumentValidationFailureDetails.spec.ts
+++ tests/unit/getDocumentValidationFailureDetails.spec.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { describe, expect, test } from '@jest/globals';
 
-import ValidationFailure from '../../types/ValidationFailure';
+import OnfidoDocumentReportBreakdown from '../../types/OnfidoDocumentReportBreakdown';
+// import ValidationFailure from '../../types/ValidationFailure';
 import ValidationResult from '../../types/ValidationResult';
 import getDocumentValidationFailureDetails from '../../utils/getDocumentValidationFailureDetails';
 
@@ -19,105 +20,104 @@ const createDocumentValidationResultPayload = ({
   dataConsistencyResult = 'clear',
   documentTypeResult = 'clear',
 }: {
-  imageIntegrityResult: ValidationResult;
-  supportedDocumentResult: ValidationResult;
-  imageQualityResult: ValidationResult;
-  visualAuthenticityResult: ValidationResult;
-  faceDetectionResult: ValidationResult;
-  fontsResult: ValidationResult;
-  securityFeaturesResult: ValidationResult;
-  dataValidationResult: ValidationResult;
-  documentNumbersResult: ValidationResult;
-  dataConsistencyResult: ValidationResult;
-  documentTypeResult: ValidationResult;
-}) => ({
-  checkId: 'c4b0061a-fcae-4796-842f-7b229911600a',
-  createdAt: '2022-10-25T12:06:32Z',
-  documents: [{ id: '0a2a80d3-5443-44c9-9770-111f9f63c180' }, { id: '27d462a5-8115-4b80-b8fc-8f0d28cf3980' }],
-  href: '/v3.4/reports/454b63da-dd90-48dd-aeba-7adb8de6a53d',
-  id: '454b63da-dd90-48dd-aeba-7adb8de6a53d',
-  name: 'document',
-  properties: {
-    dateOfBirth: '1990-01-01',
-    dateOfExpiry: '2031-05-28',
-    documentNumbers: [{ type: 'document_number', value: '999999999' }],
-    documentType: 'passport',
-    firstName: 'Kameron',
-    gender: null,
-    issuingCountry: 'GBR',
-    lastName: 'Batz',
-    nationality: null,
+  imageIntegrityResult?: ValidationResult;
+  supportedDocumentResult?: ValidationResult;
+  imageQualityResult?: ValidationResult;
+  visualAuthenticityResult?: ValidationResult;
+  faceDetectionResult?: ValidationResult;
+  fontsResult?: ValidationResult;
+  securityFeaturesResult?: ValidationResult;
+  dataValidationResult?: ValidationResult;
+  documentNumbersResult?: ValidationResult;
+  dataConsistencyResult?: ValidationResult;
+  documentTypeResult?: ValidationResult;
+}): OnfidoDocumentReportBreakdown => ({
+  ageValidation: {
+    result: 'clear',
+    breakdown: { minimumAcceptedAge: { result: 'clear', properties: {} } },
   },
-  result: 'clear',
-  status: 'complete',
-  subResult: 'clear',
-  breakdown: {
-    ageValidation: {
-      result: 'clear',
-      breakdown: { minimumAcceptedAge: { result: 'clear', properties: {} } },
-    },
-    policeRecord: { result: 'clear' },
-    imageIntegrity: {
-      result: imageIntegrityResult,
-      breakdown: {
-        colourPicture: { result: 'clear', properties: {} },
-        supportedDocument: { result: supportedDocumentResult, properties: {} },
-        conclusiveDocumentQuality: { result: 'clear', properties: {} },
-        imageQuality: { result: imageQualityResult, properties: {} },
-      },
-    },
-    dataComparison: {
-      result: 'clear',
-      breakdown: {
-        dateOfBirth: { result: 'clear', properties: {} },
-        firstName: { result: 'clear', properties: {} },
-        documentNumbers: { result: 'clear', properties: {} },
-        documentType: { result: 'clear', properties: {} },
-        lastName: { result: 'clear', properties: {} },
-        dateOfExpiry: { result: 'clear', properties: {} },
-        gender: { result: 'clear', properties: {} },
-        issuingCountry: { result: 'clear', properties: {} },
-      },
-    },
-    dataConsistency: {
-      result: 'clear',
-      breakdown: {
-        nationality: { result: 'clear', properties: {} },
-        lastName: { result: 'clear', properties: {} },
-        firstName: { result: 'clear', properties: {} },
-        multipleDataSourcesPresent: { result: 'clear', properties: {} },
-        gender: { result: 'clear', properties: {} },
-        dateOfBirth: { result: 'clear', properties: {} },
-        documentType: { result: 'clear', properties: {} },
-        issuingCountry: { result: 'clear', properties: {} },
-        documentNumbers: { result: 'clear', properties: {} },
-        dateOfExpiry: { result: 'clear', properties: {} },
-      },
-    },
-    compromisedDocument: { result: 'clear' },
-    visualAuthenticity: {
-      result: visualAuthenticityResult,
-      breakdown: {
-        faceDetection: { result: faceDetectionResult, properties: {} },
-        other: { result: 'clear', properties: {} },
-        digitalTampering: { result: 'clear', properties: {} },
-        originalDocumentPresent: { result: 'clear', properties: {} },
-        securityFeatures: { result: securityFeaturesResult, properties: {} },
-        template: { result: 'clear', properties: {} },
-        pictureFaceIntegrity: { result: 'clear', properties: {} },
-        fonts: { result: fontsResult, properties: {} },
-      },
-    },
-    dataValidation: {
-      result: 'clear',
-      breakdown: {
-        mrz: { result: 'clear', properties: {} },
-        expiryDate: { result: 'clear', properties: {} },
-        documentExpiration: { result: 'clear', properties: {} },
-        documentNumbers: { result: 'clear', properties: {} },
-        dateOfBirth: { result: 'clear', properties: {} },
-        gender: { result: 'clear', properties: {} },
-      },
+  policeRecord: { result: 'clear' },
+  imageIntegrity: {
+    result: imageIntegrityResult,
+    breakdown: {
+      colourPicture: { result: 'clear', properties: {} },
+      supportedDocument: { result: supportedDocumentResult, properties: {} },
+      conclusiveDocumentQuality: { result: 'clear', properties: {} },
+      imageQuality: { result: imageQualityResult, properties: {} },
     },
   },
+  dataComparison: {
+    result: 'clear',
+    breakdown: {
+      dateOfBirth: { result: 'clear', properties: {} },
+      firstName: { result: 'clear', properties: {} },
+      documentNumbers: { result: 'clear', properties: {} },
+      documentType: { result: 'clear', properties: {} },
+      lastName: { result: 'clear', properties: {} },
+      dateOfExpiry: { result: 'clear', properties: {} },
+      gender: { result: 'clear', properties: {} },
+      issuingCountry: { result: 'clear', properties: {} },
+    },
+  },
+  dataConsistency: {
+    result: dataConsistencyResult,
+    breakdown: {
+      nationality: { result: 'clear', properties: {} },
+      lastName: { result: 'clear', properties: {} },
+      firstName: { result: 'clear', properties: {} },
+      multipleDataSourcesPresent: { result: 'clear', properties: {} },
+      gender: { result: 'clear', properties: {} },
+      dateOfBirth: { result: 'clear', properties: {} },
+      documentType: { result: documentTypeResult, properties: {} },
+      issuingCountry: { result: 'clear', properties: {} },
+      documentNumbers: { result: 'clear', properties: {} },
+      dateOfExpiry: { result: 'clear', properties: {} },
+    },
+  },
+  compromisedDocument: { result: 'clear' },
+  visualAuthenticity: {
+    result: visualAuthenticityResult,
+    breakdown: {
+      faceDetection: { result: faceDetectionResult, properties: {} },
+      other: { result: 'clear', properties: {} },
+      digitalTampering: { result: 'clear', properties: {} },
+      originalDocumentPresent: { result: 'clear', properties: {} },
+      securityFeatures: { result: securityFeaturesResult, properties: {} },
+      template: { result: 'clear', properties: {} },
+      pictureFaceIntegrity: { result: 'clear', properties: {} },
+      fonts: { result: fontsResult, properties: {} },
+    },
+  },
+  dataValidation: {
+    result: dataValidationResult,
+    breakdown: {
+      mrz: { result: 'clear', properties: {} },
+      expiryDate: { result: 'clear', properties: {} },
+      documentExpiration: { result: 'clear', properties: {} },
+      documentNumbers: { result: documentNumbersResult, properties: {} },
+      dateOfBirth: { result: 'clear', properties: {} },
+      gender: { result: 'clear', properties: {} },
+    },
+  },
+});
+
+const successDocumentValidationResultPayload = createDocumentValidationResultPayload({});
+
+/*
+  InvalidImageIntegritySupportedDocument = 'InvalidImageIntegritySupportedDocument', // "Image Integrity - Supported Document"
+  InvalidImageIntegrityImageQuality = 'InvalidImageIntegrityImageQuality', // "Image Integrity - Image Quality"
+  InvalidVisualAuthenticity = 'InvalidVisualAuthenticity', // Generic Visual authenticity because there are many sub failures
+  InvalidVisualAuthenticityFonts = 'InvalidVisualAuthenticityFonts', // "Visual Authenticity - Fonts"
+  InvalidVisualAuthenticitySecurityFeatures = 'InvalidVisualAuthenticitySecurityFeatures', // "Visual Authenticity - Security Features"
+  InvalidVisualAuthenticityFaceDetection = 'InvalidVisualAuthenticityFaceDetection', // "Visual Authenticity - Face Detection"
+  InvalidDataValidation = 'InvalidDataValidation', // Generic Data Validation because there are many sub failures
+  InvalidDataValidationDocumentNumbers = 'InvalidDataValidationDocumentNumbers', // "Data Validation - Document Numbers"
+  InvalidDataConsistency = 'InvalidDataConsistency', // Generic Data Consistency because there are many sub failures
+  InvalidDataConsistencyDocumentType = 'InvalidDataConsistencyDocumentType', // "Data Consistency - Document Type"
+*/
+
+describe('getDocumentValidationFailureDetails', () => {
+  test('getDocumentValidationFailureDetails(successDocumentValidationResultPayload) should return an empty array', () => {
+    expect(getDocumentValidationFailureDetails(successDocumentValidationResultPayload)).toEqual([]);
+  });
 });
