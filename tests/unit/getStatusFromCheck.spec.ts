7946a825551accc05d546c61334630bcd320733d
Sandoche <sandoche@protonmail.com>
2022-11-15 14:00:03 +0000

    Created the empty test

---
+++ tests/unit/getStatusFromCheck.spec.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { describe, expect, test } from '@jest/globals';
+
+import getStatusFromCheck from '../../utils/getStatusFromCheck';
+
+describe('getStatusFromCheck', () => {});
