b6771c9b3e81052f62fb923c962640ed0496da1a
Sandoche ADITTANE <sandoche@protonmail.com>
2022-11-22 10:15:32 +0000

    Update PrivacyPolicy.tsx

--- components/privacy-policy/PrivacyPolicy.tsx
+++ components/privacy-policy/PrivacyPolicy.tsx
@@ -102,7 +102,7 @@ function PrivacyPolicy() {
       <h2>Online Privacy Policy Only</h2>
 
       <p>
-        Our Privacy Policy created at GDPRPrivacyPolicy.net) applies only to our online activities and is valid for visitors to our website with regards to the information that
+        Our Privacy Policy created at GDPRPrivacyPolicy.net applies only to our online activities and is valid for visitors to our website with regards to the information that
         they shared and/or collect in NEAR Foundation KYC. This policy is not applicable to any information collected offline or via channels other than this website.
       </p>
 
