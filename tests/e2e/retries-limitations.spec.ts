7dd12576075e9b0d32b65ca6e7896f6e185adeb3
Sandoche Adittane <sandoche@gmail.com>
2022-11-30 14:56:49 +0000

    api test added

--- tests/e2e/retries-limitations.spec.ts
+++ tests/e2e/retries-limitations.spec.ts
@@ -3,7 +3,7 @@ import { Browser, BrowserContext, chromium, expect, test } from '@playwright/tes
 
 import { CONTACT_EMAIL, COOKIE_NUMBER_OF_TRIES_NAME } from '../../constants';
 import type ApplicantProperties from '../../types/ApplicantProperties';
-import { FORBIDDEN } from '../../utils/statusCodes';
+import { FORBIDDEN, SUCCESS } from '../../utils/statusCodes';
 
 import { API_URL, FLOW_URL, MOCK_VIDEO_PATH } from './utils/constants';
 import { continueOnfidoFlowThenGetAndTestLink, fillStartForm, openKycLinkAndTestDocumentAndPhotoScan, submittingDocuments } from './utils/helpers';
@@ -82,29 +82,53 @@ test('Applicant should not be able to retry more than the maximum set up', async
 });
 
 test('/api/generate-token should return 403 if the applicant has reached the retry limit', async () => {
-  const cookies = [
-    {
-      name: COOKIE_NUMBER_OF_TRIES_NAME,
-      value: '3',
-      domain: 'localhost',
-      path: '/',
-    },
-  ];
-  desktop.addCookies(cookies);
-
   const desktopPage = await desktop.newPage();
   await desktopPage.goto(FLOW_URL);
   // eslint-disable-next-line no-underscore-dangle
   const csrfToken = await desktopPage.evaluate(() => window.__NEXT_DATA__.props.pageProps.csrfToken);
 
-  const response = await desktopPage.request.post(`${API_URL}/generate-token`, {
+  const responseTry1 = await desktopPage.request.post(`${API_URL}/generate-token`, {
     data: {
       ...applicant,
       csrf_token: csrfToken,
     },
   });
 
-  const { code, status } = await response.json();
+  await expect(responseTry1.status()).toEqual(SUCCESS);
+  await expect(await responseTry1.json()).toHaveProperty('applicantId');
+  await expect(await responseTry1.json()).toHaveProperty('sdkToken');
+
+  const responseTry2 = await desktopPage.request.post(`${API_URL}/generate-token`, {
+    data: {
+      ...applicant,
+      csrf_token: csrfToken,
+    },
+  });
+
+  await expect(responseTry2.status()).toEqual(SUCCESS);
+  await expect(await responseTry2.json()).toHaveProperty('applicantId');
+  await expect(await responseTry2.json()).toHaveProperty('sdkToken');
+
+  const responseTry3 = await desktopPage.request.post(`${API_URL}/generate-token`, {
+    data: {
+      ...applicant,
+      csrf_token: csrfToken,
+    },
+  });
+
+  await expect(responseTry3.status()).toEqual(SUCCESS);
+  await expect(await responseTry3.json()).toHaveProperty('applicantId');
+  await expect(await responseTry3.json()).toHaveProperty('sdkToken');
+
+  const responseTry4 = await desktopPage.request.post(`${API_URL}/generate-token`, {
+    data: {
+      ...applicant,
+      csrf_token: csrfToken,
+    },
+  });
+
+  await expect(responseTry4.status()).toEqual(FORBIDDEN);
+  const { code, status } = await responseTry4.json();
 
   expect(code).toBe(FORBIDDEN);
   expect(status).toBe('Maximum number of tries reached');
